3ba1235f9b1f48bc218e14cb16786c82b62ec159
menghanhui <menghanhui95@gmail.com>
2016-10-29 13:43:05 -0400

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -3,20 +3,39 @@ angular.module('portalApp')
 .controller('skylarCtrl', ['$scope', '$http', '$q', 'skylarFactory', function ($scope, $http, $q, skylarFactory) {
 
     // Import variables and functions from service
-    $scope.item = [
-		{			
-			tags: ['tag A', 'tag B', 'tag C'],			
-		}];
-
+    $scope.item ={value:''};
+    $scope.item2 = {value:''};
+    $scope.item3 = {value:''};
+    $scope.item4 = {value:''};
+    
+     
+    
     // Model for the search and list example
     $scope.model = {value: [{
         title: "Campus Tech",
+        
+        location: "Location: Student Life Center Downstairs",
+        hours:"Regular Hours: Monday-Friday, 9a.m-5p.m",  
+        
+        details: [    
+        {title: "Notebooks",
         details: "item 1 details",
+        category: '2'},
+            
+        {title: "Pensils",
+        details: "item 1 details",
+        category: '2'},
+            
+         {title: "Pens",
+        details: "item 1 details",
+        category: '2'}        
+                            ],
+        
         category: '1'
     }, {
         title: "Waterloo Store",
         details: "item 2 details",
-        category: '2'
+        category: '1'
     }, {
         title: "Write Stuff",
         details: "item 3 details",
@@ -24,7 +43,7 @@ angular.module('portalApp')
     }, {
         title: "International News",
         details: "item 4 details",
-        category: '2'
+        category: '1'
     }, {
         title: "Media.doc",
         details: "item 5 details",
@@ -43,6 +62,15 @@ angular.module('portalApp')
         $scope.portalHelpers.showView('skylarDetails.html', 2);
     };
 
+     // Handle click on an item in the second list 
+ $scope.showDetails2 = function (item) {
+        console.log(item);
+        // Set which item to show in the details view
+        $scope.item2.value = item;
+        // Show details view in the second column
+        $scope.portalHelpers.showView('skylarDetails2.html', 3);
+    };
+    
     // Handle "previous item" click from the details page
     $scope.prevItem = function () {
         // get previous items in the list
@@ -57,6 +85,8 @@ angular.module('portalApp')
     }
 
 }])
+
+
     // Factory maintains the state of the widget
     .factory('skylarFactory', ['$http', '$rootScope', '$filter', '$q', function ($http, $rootScope, $filter, $q) {
         var initialized = {
